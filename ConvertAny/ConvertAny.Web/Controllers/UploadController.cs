ee91b66d93834871585522a5b1be241069ac1fad
Andy Chang <andyta24@gmail.com>
2021-01-19 09:54:19 +0800

    Add Upload Page Add Razor Refresh Page

---
+++ ConvertAny/ConvertAny.Web/Controllers/UploadController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConvertAny.Web.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}

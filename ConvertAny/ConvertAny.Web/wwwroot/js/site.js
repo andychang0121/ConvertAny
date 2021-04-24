23ff2fd471483b0c78a2384454c14cc14de222d2
安迪學長 <andyta24@gmail.com>
2021-04-24 16:51:11 +0800

    Tidy Codes - Layout

--- ConvertAny/ConvertAny.Web/wwwroot/js/site.js
+++ ConvertAny/ConvertAny.Web/wwwroot/js/site.js
@@ -30,3 +30,95 @@ function validFileSize(bytes) {
     }
     return _getSize(bytes) <= 2;
 }
+
+document.body.addEventListener("touchstart", function (e) { e.preventDefault(); });
+document.body.addEventListener("touchmove", function (e) { e.preventDefault(); });
+
+Number.prototype.numberFormat = function (c, d, t) {
+    var n = this,
+        c = isNaN(c = Math.abs(c)) ? 2 : c,
+        d = d == undefined ? "." : d,
+        t = t == undefined ? "," : t,
+        s = n < 0 ? "-" : "",
+        i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+function getFileBase64(file) {
+    return new window.Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+function getImage(src) {
+    return new window.Promise((resolve, revoke) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.crossOrigin = "Anonymous";
+        img.src = src;
+    });
+}
+
+function getDownloadFile(fileName, blob, type) {
+    return new window.Promise((resolve, reject) => {
+        const _type = type === undefined ? "application/zip" : type;
+        const binaryString = window.atob(blob);
+        const binaryLen = binaryString.length;
+        const bytes = new Uint8Array(binaryLen);
+
+        for (let i = 0; i < binaryLen; i++) {
+            const ascii = binaryString.charCodeAt(i);
+            bytes[i] = ascii;
+        }
+
+        const url = window.URL.createObjectURL(new Blob([bytes], { type: _type }));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+        resolve(link);
+    });
+}
+
+function getUploadFiles(imgs) {
+    const rs = [];
+    [].forEach.call(imgs, function (img) {
+        const _isUpload = validFileSize(img.file.size);
+        if (_isUpload) {
+            rs.push(img);
+        }
+    });
+    return rs;
+}
+
+function getCustomSize() {
+    const rs = {
+        customWidth: document.getElementById("customWidth").value,
+        customHeight: document.getElementById("customHeight").value
+    }
+    return rs;
+}
+
+function getParentNode(o, name) {
+    const _tagName = o.tagName.toLowerCase();
+    if (_tagName === name) return o;
+    return getParentNode(o.parentNode, name);
+}
+
+function findArray(a, v) {
+    if (!Array.isArray(a)) return false;
+    return a.indexOf(v) >= 0;
+}
+
+function getParentNodeBySelector(o, name) {
+    name = name.replace("[", "").replace("]", "").replace("data", "").replace("-", "").toLowerCase();
+    if (o === null || o === undefined || o.dataset === undefined) return undefined;
+    const _dataSet = Object.keys(o.dataset);
+    if (findArray(_dataSet, name)) return o;
+    return getParentNodeBySelector(o.parentNode, name);
+}
